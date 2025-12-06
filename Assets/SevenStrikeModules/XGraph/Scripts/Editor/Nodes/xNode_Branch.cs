77ab51bff69295d3e1b54bfccc65a1e0d1bfe9b7
SevenStrike <2110481973@qq.com>
2025-12-06 22:57:11 +0800

    update

--- Assets/SevenStrikeModules/XGraph/Scripts/Editor/Nodes/xNode_Branch.cs
+++ Assets/SevenStrikeModules/XGraph/Scripts/Editor/Nodes/xNode_Branch.cs
@@ -1,14 +1,17 @@
 namespace SevenStrikeModules.XGraph
 {
+    using UnityEditor;
     using UnityEditor.Experimental.GraphView;
     using UnityEngine;
+    using UnityEngine.UIElements;
 
     public class xNode_Branch : xNode_Base
     {
+        internal xAction_Branch branch;
+
         public override void Initialize(xg_GraphView graphView, Vector2 pos = default, xAction_Base data = null)
         {
             base.Initialize(graphView, pos, data);
-
 
             #region 端口设置
             // 加入行为端口
@@ -17,6 +20,8 @@ namespace SevenStrikeModules.XGraph
             Port_Outputs.Add(new xGraph_NodePort("开", typeof(xAction_Base), Port.Capacity.Single));
             Port_Outputs.Add(new xGraph_NodePort("关", typeof(xAction_Base), Port.Capacity.Single));
             #endregion          
+
+            branch = data as xAction_Branch;
         }
 
         #region 节点绘制
@@ -68,7 +73,7 @@ namespace SevenStrikeModules.XGraph
         }
         #endregion
 
-        #region 回调
+        #region 重写 - 回调
         /// <summary>
         /// 黑板变量数值变化时的回调
         /// </summary>
@@ -76,9 +81,80 @@ namespace SevenStrikeModules.XGraph
         {
             base.On_VariablesValue_Changed();
 
-            xAction_Branch branch = ActionData as xAction_Branch;
             branch.SetPredicateState("条件");
         }
-        #endregion       
+        #endregion
+
+        //#region 重写 - 绘制Inspector
+        ///// <summary>
+        ///// 子行为折叠容器
+        ///// </summary>
+        ///// <param name="root"></param>
+        //public override Foldout ins_Folder_ChildActions(VisualElement root)
+        //{
+        //    Foldout fold = base.ins_Folder_ChildActions(root);
+        //    fold.text = $"{fold.text}";
+
+        //    xAction_Base child_true = branch.BaseArgs.RootAsset.FindActionNode(branch.childNode_true);
+
+        //    VisualElement container = new VisualElement();
+        //    container.AddToClassList("list_container");
+        //    fold.Add(container);
+
+        //    container.RegisterCallback<PointerEnterEvent>((evt) =>
+        //    {
+        //        xg_Window wnd = util_XGraphEditorUtility.GetGraphviewWindow();
+        //        Node node = wnd.xw_graphView.FindNode(child_true.BaseArgs.guid);
+        //        if (node is xNode_Base n_base)
+        //        {
+        //            n_base.Highlight();
+        //        }
+        //    });
+
+        //    container.RegisterCallback<PointerLeaveEvent>((evt) =>
+        //    {
+        //        xg_Window wnd = util_XGraphEditorUtility.GetGraphviewWindow();
+        //        Node node = wnd.xw_graphView.FindNode(child_true.BaseArgs.guid);
+        //        if (node is xNode_Base n_base)
+        //        {
+        //            n_base.UnHighlight();
+        //        }
+        //    });
+
+        //    VisualElement container_title = new VisualElement();
+        //    container_title.AddToClassList("list_titlebg");
+        //    container.Add(container_title);
+
+        //    VisualElement container_icon = new VisualElement();
+        //    container_icon.AddToClassList("list_item_icon");
+        //    container_icon.style.backgroundImage = child_true.BaseArgs.NodeIcon == null ? util_XGraphEditorUtility.AssetLoad<Texture2D>(AssetDatabase.GUIDToAssetPath(child_true.BaseArgs.icon)) : child_true.BaseArgs.NodeIcon;
+        //    container_title.Add(container_icon);
+
+        //    util_XGraphInspectorGUI.GUI_Label(container_title, $"目标：{child_true.identifyName}", new string[] { "labeltext", "list_item_title" });
+        //    util_XGraphInspectorGUI.GUI_Label(container_title, "行为", new string[] { "list_item_marktext" });
+        //    util_XGraphInspectorGUI.GUI_Label(container, $"<b>Guid：</b><color=#e1e1e1>{child_true.BaseArgs.guid}</color>", new string[] { "list_item_label" });
+        //    util_XGraphInspectorGUI.GUI_Label(container, $"<b>行为类型：</b><color=#e1e1e1>{child_true.BaseArgs.actionNodeType}</color>", new string[] { "list_item_label" });
+        //    util_XGraphInspectorGUI.GUI_Label(container, $"<b>节点类型：</b><color=#e1e1e1>{child_true.BaseArgs.visualNodeType}</color>", new string[] { "list_item_label" });
+
+        //    return fold;
+        //}
+        ///// <summary>
+        ///// 属性节点的属性项折叠容器
+        ///// </summary>
+        ///// <param name="root"></param>
+        ///// <returns></returns>
+        //public override Foldout ins_Folder_Propertys(VisualElement root)
+        //{
+        //    return null;
+        //}
+        ///// <summary>
+        ///// 自定义组件折叠容器
+        ///// </summary>
+        ///// <param name="root"></param>
+        //public override Foldout ins_Folder_Extensions(VisualElement root)
+        //{
+        //    return null;
+        //}
+        //#endregion
     }
 }
